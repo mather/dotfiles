07e87202a539765ccb9312222e7e053523828927
mather <mather314@gmail.com>
2014-11-21 21:57:09 +0900

    Disable coreutils.

--- home/.zshrc.d/coreutils.sh
+++ home/.zshrc.d/coreutils.sh
@@ -1,2 +1,2 @@
-export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
-export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
+#export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+#export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
